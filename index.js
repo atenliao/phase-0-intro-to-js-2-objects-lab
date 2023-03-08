ddf0aefb05358efce1b97d42deb923eab3d8d7b2
Zhongming <atenliao@gmail.com>
2023-03-07 23:08:14 -0800

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Jayco",
+    streeAddress: "413 Pork",
+    city: "Neward",
+    zipcode: "95101",
+}
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return { ...obj, [key]: value, };
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value;
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj,key){
+    return {...obj[key]};
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key];
+    return obj;
+}
